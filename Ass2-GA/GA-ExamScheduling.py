80c964fd3311194ca8c00f727f78de655edee1b7
harris <harryaamir073@gmail.com>
2023-03-25 23:07:44 +0500

    VFinal

--- Ass2-GA/GA-ExamScheduling.py
+++ Ass2-GA/GA-ExamScheduling.py
@@ -1,33 +1,34 @@
+# HARRIS AAMIR 
+# 20i0943 SE-S 
 import random
 import time
 start_time = time.time()
-
-N=0 
-K=0 
-T=0 
-slotDuration= 0  
-PopultaionSize=100 
-cources=[]  
-cources_duration=[] 
-halls=[]
-timeSlots=[]
-population=[]
-commonStu=[]
-
-index_fitness=[] #list that contains indexs and fitness values of population 
+N=0                     #number of courses
+K=0                     #number of exam halls
+T=0                     #number of slots
+totalHallTime= 0        #total time for hall
+PopultaionSize=100      #size of population
+courses=[]              #list of all courses with respect to index
+courses_duration=[]     #list of all courses time with respect to index
+halls=[]                #list of all halls (1,2,3..)
+timeSlots=[]            #list of all timeSlots (1,2,3...)
+population=[]           #list of all possible solutions
+commonStu=[]            #list tuples containing (course, course, studentNum)
+index_fitness=[]        #list that contains indexs and fitness values of population 
 
 #-----INPUT-------
-N=int(input(">Enter the number of cources (N): "))
+print("\n=========ASSIGNMENT 2==========")
+N=int(input("\n>Enter the number of course (N): "))
 
 for i in range(N):
-    cources.append(input(">>Enter name of cource #"+str((i+1))+": "))
-    cources_duration.append(input(">>>Enter duration for this cource exam (hours): "))
+    courses.append(input(">>Enter name of course #"+str((i+1))+": "))
+    courses_duration.append(int(input(">>>Enter duration for this course exam (hours): ")))
 
 K=int(input("\n>Enter the number of exam halls (K): "))
+totalHallTime= int(input(">>Enter the total time avalible for halls (hours): "))
 
-T=int(input(">>Enter the number of slots avaliable in each hall: "))
- 
-slotDuration= int(input(">>>Enter the duration of one timeslot (hours): "))
+T=int(input("\n>Enter the number of slots avaliable in each hall: "))
+
 print("\n")
 for i in range(K):
     halls.append(str(i))
@@ -35,20 +36,20 @@ for i in range(T):
     timeSlots.append(str(i))
 for i in  range(N):
     for j in range(i+1, N):
-        commonStu.append((cources[i], cources[j], int(input(">Enter the number of common students in "+cources[i]+" and "+cources[j]+": "))))
-if K*T<N:
+        commonStu.append((courses[i], courses[j], int(input(">Enter the number of common students in "+courses[i]+" and "+courses[j]+": "))))
+if K*T<N: #number of courses are less then the avaliable slots
     print("----------------------------------------------------------") 
-    print("|  Solution not possible as slots are less then cources  |")
+    print("|  Solution not possible as slots are less then courses  |")
     print("----------------------------------------------------------") 
     exit()
 #------GENERATING RANDOM POPULATION------
 def genPopulation():
     for i in range(PopultaionSize):
         chromosom=[]
-        courcesCopy=cources[:]
+        coursesCopy=courses[:]
         for i in range(N):
-            c = random.choice(courcesCopy)
-            courcesCopy.remove(c)
+            c = random.choice(coursesCopy)
+            coursesCopy.remove(c)
             t=0
             k=0
             t= random.choice(timeSlots)
@@ -59,53 +60,56 @@ def genPopulation():
 
 #-------CONSTRAINTS CHECK---------
 
-def getFromTime(chromosom, time): # returns the list of cources on same time
-    courceslist=[]
+def getFromTime(chromosom, time): # returns the list of courses on same time
+    courseslist=[]
     for i in range(N):
        if chromosom[i][1]==time:
-           courceslist.append(chromosom[i][0])
-    return courceslist
+           courseslist.append(chromosom[i][0])
+    return courseslist
 
-def getStudentClashesCount(courceslist): #returns the count of students with clashes between the cources of courcelist
+def getStudentClashesCount(courseslist): #returns the count of students with clashes between the courses of courcelist
     for i in range(len(commonStu)):
-        if commonStu[i][0]==courceslist[0] and commonStu[i][1]==courceslist[1]:
+        if commonStu[i][0]==courseslist[0] and commonStu[i][1]==courseslist[1]:
             return commonStu[i][2]
-        elif commonStu[i][0]==courceslist[1] and commonStu[i][1]==courceslist[0]:
+        elif commonStu[i][0]==courseslist[1] and commonStu[i][1]==courseslist[0]:
             return commonStu[i][2]
     return 0
 def getCourceTime(cource):
-    for i in range(len(cources)):
-        if cources[i]==cource:
-            return cources_duration[i]
+    for i in range(len(courses)):
+        if courses[i]==cource:
+            return courses_duration[i]
     return -1
 
 #-----FITNESS FUNCTION-----------
 def fitness(chromosom):
     slotslist=[] #contains all possible slots of halls
-    courcesList=[] # saves all cources in the chromosoms
+    coursesList=[] # saves all courses in the chromosoms
     fitnessVal=0 
     for i in range(N):
-        courcesList.append(chromosom[i][0])
+        coursesList.append(chromosom[i][0])
     
     for i in range(N):
-        if cources[i] not in courcesList:
-            return 4321 #chromosom have reperting cource
+        if courses[i] not in coursesList:
+            return 4000 #chromosom have reperting cource
 
     if len(chromosom)!=N:
-        return 1234 # shows that chromosome is missing a cource 
+        return 4000 # shows that chromosome is missing a cource 
     
-    #checking for clashes of slots (2 cources have same time and same location )
+    #checking for clashes of slots (2 courses have same time and same location )
     for i in range(N):
      val=str(chromosom[i][1]) + str(chromosom[i][2])
      if val not in slotslist: 
          slotslist.append(val)
      else:
-         return 1000  # checking no 2 cources have same time and same hall  
+         return 1000  # checking no 2 courses have same time and same hall  
     
-    for i in chromosom:
-       endingTime= int(getCourceTime(i[0]))
-       if endingTime>slotDuration:
-           fitnessVal+= 10*(endingTime-slotDuration)
+    for hall in halls:
+        halltime=0
+        for sol in chromosom:
+            if sol[2] == hall:
+                halltime+=int(getCourceTime(sol[0]))
+        if halltime>totalHallTime:
+            fitnessVal+=10*(halltime-totalHallTime)
 
     #checking for clashes students 
     times=[chromosom[0][1]]
@@ -113,8 +117,8 @@ def fitness(chromosom):
         if chromosom[i][1] not in times:
             times.append(chromosom[i][1]) 
         else:
-            courcesList=getFromTime(chromosom,chromosom[i][1])
-            count= getStudentClashesCount(courcesList)
+            coursesList=getFromTime(chromosom,chromosom[i][1])
+            count= getStudentClashesCount(coursesList)
             fitnessVal+=count*100 # for every clashed student, 100 point are added
 
     return fitnessVal
@@ -144,7 +148,7 @@ def crossover(population):
 
 #---MUTATION FUNCTION------
 def mutation(population):
-    for i in range(0,len(population)-1,4):
+    for i in range(0,len(population)-1,10):
         index=random.randint(0, N-1)
         chromosom1=population[i][:]
         chromosom2=population[i+1][:]
@@ -169,16 +173,16 @@ def evaluate(population,length): # returns the best chromosoms from population
         pop.append(population[indexList[i][0]])
     return pop
 
-#----CREATES A NEW POPULATION-----
+#----CREATES A NEW GENERATION -----
 def nextGen():
-  #new population contains 50% old 40% crossover and 10% mutation
-  crossoverPop=evaluate(crossover(population),int(PopultaionSize*0.4))
+  #new population contains 10% old 80% crossover and 10% mutation
+  crossoverPop=evaluate(crossover(population),int(PopultaionSize*0.8))
   mutationPop=evaluate(mutation(population),int(PopultaionSize*0.1))
   Pop=[]
   index_fitness.sort(key=takeFitness)
-  for i in range(int(PopultaionSize*0.5)):
+  for i in range(int(PopultaionSize*0.1)):
       Pop.insert(0,population[index_fitness[i][0]])
-  for i in range(int(PopultaionSize*0.4)):
+  for i in range(int(PopultaionSize*0.8)):
       Pop.insert(0,crossoverPop[i])
   for i in range(int(PopultaionSize*0.1)):
        Pop.insert(0,mutationPop[i])
@@ -196,7 +200,6 @@ def main():
         index_fitness=[]
         for j in range(PopultaionSize):
             index_fitness.append([j,fitness(nextgen[j])])
-        # print("Best Solution of this generation: \n"+str(nextgen[index_fitness[0][0]])+"With fitness"+str(index_fitness[0][1]))
         index_fitness.sort(key=takeFitness)
         population=nextgen
     print("Total Generations: 100")
@@ -211,4 +214,4 @@ def main():
     
 main()
 
-print("---Running Time: %s seconds ---" % (time.time() - start_time))
+print("---Running Time: %s seconds-----" % (time.time() - start_time))
